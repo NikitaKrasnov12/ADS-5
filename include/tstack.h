9451105e8854d13211a3500efb1f45b2212394d0
NikitaKrasnov12 <116708902+NikitaKrasnov12@users.noreply.github.com>
2023-04-10 03:41:22 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,39 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T arr[100];
+    int top_index;
+ 
+
+ public:
+    TStack() :top_index(-1) {}
+    void push(T value) {
+        if (isFull()) {
+            throw std::string("Stack is full");
+        } else {
+            arr[++top_index] = value;
+        }
+    }
+    const T& pop() {
+        if (isEmpty()) {
+            throw std::string("Stack is empty");
+        } else {
+            return arr[top_index--];
+        }
+    }
+    bool isEmpty() const {
+        return top_index == -1;
+    }
+    bool isFull() const {
+        return top_index == size - 1;
+    }
+    const T& get() const {
+        return arr[top_index];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
